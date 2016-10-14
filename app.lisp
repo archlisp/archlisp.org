cf4013d9c7191b942d2c03d347c083ec429fa5c6
Christopher Mark Gore <cgore@cgore.com>
2016-10-13 19:54:07 -0500

    Basic page

--- app.lisp
+++ app.lisp
@@ -1,3 +1,6 @@
 (lambda (env)
   (declare (ignore env))
-  '(200 (:content-type "text/plain") ("Hello, Clack!")))
+  '(200 (:content-type "text/plain")
+        ("Arch Lisp needs a website.
+On Meetup: https://www.meetup.com/Arch-Lisp
+On Github: https://github.com/archlisp")))
